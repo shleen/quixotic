f6802cc707706f059b2b9acf4e1ea460e739806f
Sheline Sim <sim.sheline@gmail.com>
2019-06-03 17:00:05 +0800

    Pull & dump all words from Firebase

--- app/src/main/java/com/shleen/quixotic/MainActivity.java
+++ app/src/main/java/com/shleen/quixotic/MainActivity.java
@@ -1,13 +1,75 @@
 package com.shleen.quixotic;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("words");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadData();
+    }
+
+    private void loadData() {
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                // Create array of definitions
+                ArrayList<Definition> definitions = new ArrayList<>();
+                for (DataSnapshot child : dataSnapshot.child("definitions").getChildren()) {
+
+                    definitions.add(child.getValue(Definition.class));
+
+                }
+
+                // Create word
+                Word word = new Word(dataSnapshot.child("word").getValue().toString(),
+                                     dataSnapshot.child("phonetic").getValue().toString(),
+                                     definitions);
+
+                TextView txtExample = (TextView) findViewById(R.id.txtExample);
+                txtExample.setText(txtExample.getText() + word.getWord());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d("FIREBASE", "onChildChanged:" + dataSnapshot.getKey());
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
