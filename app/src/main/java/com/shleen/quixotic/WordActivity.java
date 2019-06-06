a4b95ce52a5d5f53f778497cc538ed3679ee1e0b
Sheline Sim <sim.sheline@gmail.com>
2019-06-06 11:17:12 +0800

    Remove title bar from application

--- app/src/main/java/com/shleen/quixotic/WordActivity.java
+++ app/src/main/java/com/shleen/quixotic/WordActivity.java
@@ -2,7 +2,6 @@ package com.shleen.quixotic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
