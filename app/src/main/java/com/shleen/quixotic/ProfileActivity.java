502bb72bcb6c9a8414e8f2e43552141bb1765f35
shleen <sim.sheline@gmail.com>
2020-04-20 00:38:27 +0800

    starting work for profile

--- app/src/main/java/com/shleen/quixotic/ProfileActivity.java
+++ app/src/main/java/com/shleen/quixotic/ProfileActivity.java
@@ -4,8 +4,11 @@ import androidx.annotation.NonNull;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -15,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 public class ProfileActivity extends BaseActivity {
 
     GoogleSignInClient mGoogleSignInClient;
+    ImageView img_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,15 @@ public class ProfileActivity extends BaseActivity {
 
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Set img_user
+        img_user = (ImageView) findViewById(R.id.img_user);
+        String img_user_url = GoogleSignIn.getLastSignedInAccount(this).getPhotoUrl().toString();
+        Log.i("QUIXOTIC_TEST", "in");
+        Log.i("QUIXOTIC_TEST", img_user_url);
+
+        Glide.with(this).load(img_user_url).into(img_user);
+
     }
 
     public void attemptSignOut(final View v) {
